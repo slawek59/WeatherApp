54340b311479491daad3d56a5ad54823df938221
Slawomir Was <was.slawek@gmail.com>
2024-01-02 12:56:06 +0100

    First Commit; add MVC folders, wwwroot folder, StyleSheet.css, HomeController and Index.cshtml

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("/")]
+		[Route("home")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
