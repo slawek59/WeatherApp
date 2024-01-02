4473d47900acf6a749136ecff5886ac4ea827874
Slawomir Was <was.slawek@gmail.com>
2024-01-02 16:04:06 +0100

    Add Details.cshtml

--- WeatherApp/Controllers/HomeController.cs
+++ WeatherApp/Controllers/HomeController.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
 
 namespace WeatherApp.Controllers
 {
 	public class HomeController : Controller
 	{
+		private List<CityWeather> cityWeatherList = new List<CityWeather>()
+			{
+				new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureFahrenheit = 33},
+
+				new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureFahrenheit = 60},
+
+				new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureFahrenheit = 82}
+			};
+
 		[Route("/")]
 		[Route("home")]
 		public IActionResult Index()
 		{
-			return View();
+			
+
+			//ViewData["cityWeatherList"] = cityWeatherList;
+			//ViewBag.CityWeatherList = cityWeatherList;
+
+			return View(cityWeatherList);
+		}
+
+		[Route("weather/test")]
+		public IActionResult Details()
+		{
+			
+			return View(cityWeatherList[0]);
 		}
 	}
 }
