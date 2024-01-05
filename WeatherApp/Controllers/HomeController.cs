8e549a1a3c3edfae6b8a67a49a5a4af2f5754ce3
Slawomir Was <was.slawek@gmail.com>
2024-01-05 16:32:54 +0100

    9. Add Layout Views

--- WeatherApp/Controllers/HomeController.cs
+++ WeatherApp/Controllers/HomeController.cs
@@ -24,28 +24,27 @@ namespace WeatherApp.Controllers
 		[Route("home")]
 		public IActionResult Index()
 		{
-			//ViewData["cityWeatherList"] = cityWeatherList;
-			//ViewBag.CityWeatherList = cityWeatherList;
-
 			return View(cityWeatherList);
 		}
 
-		[Route("weather/{cityCode}")]
-		public IActionResult Details()
+		[Route("weather/{cityCode?}")]
+		public IActionResult Details(string cityCode)
 		{
-			var cityCode = HttpContext.Request.RouteValues["cityCode"];
-
-			foreach (CityWeather item in cityWeatherList)
+			if (string.IsNullOrEmpty(cityCode))
 			{
-				if (item.CityUniqueCode == cityCode.ToString())
-				{
-					return View(item);
-				}
+				HttpContext.Response.StatusCode = 404;
+				return View("ErrorPage");
 			}
 
-			HttpContext.Response.StatusCode = 404;
-			return View("ErrorPage"); // should it return 404??
-									  //return NotFound("WRONG CITY CODE!!!!");
+			CityWeather? city = cityWeatherList.Where(temp => temp.CityUniqueCode == cityCode).FirstOrDefault();
+
+			if (city == null)
+			{
+				HttpContext.Response.StatusCode = 404;
+				return View("ErrorPage");
+			}
+
+			return View(city);
 		}
 	}
 }
